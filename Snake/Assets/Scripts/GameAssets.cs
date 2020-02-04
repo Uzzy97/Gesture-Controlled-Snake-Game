5ef083325055064a85adbf15c51178d7139a4e00
Usman Sattar <G00345816@gmit.ie>
2020-02-04 13:34:43 +0000

    Snake Movement Working

---
+++ Snake/Assets/Scripts/GameAssets.cs
@@ -0,0 +1,21 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CodeMonkey;
+using CodeMonkey.Utils;
+
+
+
+    public class GameAssets : MonoBehaviour
+{
+
+    public static GameAssets i;
+
+    private void Awake() {
+        i = this;
+    }
+
+    public Sprite snakeHeadSprite;
+
+}
